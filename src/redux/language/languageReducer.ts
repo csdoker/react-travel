770bf52ad718356e3cb838c9912b62b18c0f0c2b
csdoker <csd758371536@qq.com>
2021-02-16 16:40:30 +0800

    feat: action的拆分与统一

--- src/redux/language/languageReducer.ts
+++ src/redux/language/languageReducer.ts
@@ -1,4 +1,5 @@
 import i18n from 'i18next'
+import { CHANGE_LANGUAGE, ADD_LANGUAGE, LanguageActionTypes } from './languageActions'
 
 export interface LanguageState {
   language: 'en' | 'zh'
@@ -22,12 +23,12 @@ const defaultState: LanguageState = {
   ]
 }
 
-const languageReducer = (state = defaultState, action) => {
+const languageReducer = (state = defaultState, action: LanguageActionTypes) => {
   switch (action.type) {
-    case 'change_language':
+    case CHANGE_LANGUAGE:
       i18n.changeLanguage(action.payload)
       return { ...state, language: action.payload }
-    case 'add_language':
+    case ADD_LANGUAGE:
       return {...state, languageList: [...state.languageList, action.payload]}
     default:
       return state

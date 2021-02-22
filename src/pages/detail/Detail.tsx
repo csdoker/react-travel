bc933650adda15d3a890982496ea6bddd7fb9050
csdoker <csd758371536@qq.com>
2021-02-22 22:36:21 +0800

    feat: 使用redux-toolkit重构Detail.tsx

--- src/pages/detail/Detail.tsx
+++ src/pages/detail/Detail.tsx
@@ -15,6 +15,9 @@ import {
 } from 'antd'
 import { Header, Footer, ProductIntro, ProductComments } from '../../components'
 import { commentMockData } from './mockup'
+import { productDetailSlice } from '../../redux/productDetail/slice'
+import { useSelector } from '../../redux/hooks'
+import { useDispatch } from 'react-redux'
 
 const { RangePicker } = DatePicker
 
@@ -24,24 +27,27 @@ interface MatchParams {
 
 export const Detail: React.FC<RouteComponentProps<MatchParams>> = props => {
   const { touristRouteId } = useParams<MatchParams>()
-  const [loading, setLoading] = useState<boolean>(true)
-  const [product, setProduct] = useState<any>(null)
-  const [error, setError] = useState<string | null>(null)
+  // const [loading, setLoading] = useState<boolean>(true)
+  // const [product, setProduct] = useState<any>(null)
+  // const [error, setError] = useState<string | null>(null)
+  const loading = useSelector(state => state.productDetail.loading)
+  const error = useSelector(state => state.productDetail.error)
+  const product = useSelector(state => state.productDetail.data)
+  const dispatch = useDispatch()
 
   useEffect(() => {
     const fetchData = async () => {
-      setLoading(true)
+      dispatch(productDetailSlice.actions.fetchStart())
       try {
         const { data } = await axios.get(
           `http://123.56.149.216:8080/api/touristRoutes/${touristRouteId}`
         )
-        setProduct(data)
-        setLoading(false)
+        dispatch(productDetailSlice.actions.fetchSuccess(data))
       } catch (error) {
-        setError(error.message)
-        setLoading(false)
+        dispatch(productDetailSlice.actions.fetchFail(error.message))
       }
     }
+    fetchData()
   }, [])
   if (loading) {
     return (
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,15 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import languageReducer from './language/languageReducer'
 import recommendProductsReducer from './recommendProducts/recommendProductsReducer'
 import { actionLog } from './middleware/actionLog'
+import { productDetailSlice } from './productDetail/slice'
+import { combineReducers } from '@reduxjs/toolkit'
 
 const rootReducer = combineReducers({
   language: languageReducer,
-  recommendProducts: recommendProductsReducer
+  recommendProducts: recommendProductsReducer,
+  productDetail: productDetailSlice.reducer
 })
 
 const store = createStore(rootReducer, applyMiddleware(thunk, actionLog))

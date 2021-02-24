ea5c2d62f9877ec5515d94c32092f2cf9a38deb6
csdoker <csd758371536@qq.com>
2021-02-24 22:52:38 +0800

    feat: 完成用户注册

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import styles from './Register.module.css'
 import { UserLayout } from '../../layouts/userLayout'
+import { RegisterForm } from './RegisterForm'
 
 export const Register: React.FC = () => {
   return (
     <UserLayout>
-      <h1>register</h1>
+      <RegisterForm />
     </UserLayout>
   )
 }
